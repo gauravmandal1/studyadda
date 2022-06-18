a7d3676cdaf10ea42ecba160170aa1e2f8b04fd3
gauravmandal1 <64638825+gauravmandal1@users.noreply.github.com>
2022-06-18 23:11:53 +0530

    final project

---
+++ base/forms.py
@@ -0,0 +1,20 @@
+from django.forms import ModelForm
+from .models import Room,User
+from django.contrib.auth.forms import UserCreationForm
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields=['name','username','email','password1','password2']
+
+class RoomForm(ModelForm):
+    class Meta:
+        model = Room    #auto creation of form fields
+        fields = '__all__'
+        exclude=['host','participants']
+         
+class UserForm(ModelForm):
+    class Meta:
+        model=User
+        fields=['avatar','name','username','email','bio']
+    
---
+++ studybud/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static 
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('base.urls')),
+    path('api/',include('base.api.urls'))
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+]
+
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
