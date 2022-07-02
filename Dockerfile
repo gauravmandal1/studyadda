f8817cff581e6e1fdf43b9a3211d7c4c2b0e4f71
gauravmandal1 <64638825+gauravmandal1@users.noreply.github.com>
2022-07-02 19:28:11 +0530

    first_try

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM python:3.8-alpine
- 
-ENV PATH="/scripts:${PATH}"
-COPY ./requirements.txt /requirements.txt
-RUN apk add --update --no-chache --virtual .tmp gcc libc-dev linux-headers
-RUN pip install -r requirements.txt
--- studybud/settings.py
+++ studybud/settings.py
@@ -13,20 +13,23 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+SECRET_KEY = 'django-insecure-vuwb9mlpld=680z!(i%^t#7lqdg%ypvu_38gz&$v=@4pphvx-j'
+DEBUG=True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vuwb9mlpld=680z!(i%^t#7lqdg%ypvu_38gz&$v=@4pphvx-j'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# SECRET_KEY = 'django-insecure-vuwb9mlpld=680z!(i%^t#7lqdg%ypvu_38gz&$v=@4pphvx-j'
+# # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 
-ALLOWED_HOSTS = ['https://study-addaa.herokuapp.com/','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
